29952ef0c91047fec2b26d058d0a9b3b93b86f17
Carol <carolbrandes23@gmail.com>
2020-02-17 22:25:54 -0300

    header finalizado

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tela Login Facebook</title>
+        <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+        <header class="logo-e-acesso-container">
+                <div class="contexto logo-e-acesso-flex">
+                        <h1>Facebook</h1>
+
+                        <form action="" method="POST">
+                                <div>
+                                        <label for="login">Email ou Telefone</label>
+                                        <input type="text" name="login" id="login" required autofocus placeholder="Email ou Telefone">
+                                </div>
+
+                                <div>
+                                        <label for="senha">Senha</label>
+                                        <input type="password" name="senha" id="senha" required placeholder="Senha">
+                                        <span><a href="">Esqueceu a conta?</a></span>
+                                </div>
+
+                                <button class="botao-entrar" name="acao" type="submit"> Entrar</button>
+
+                        </form>
+                </div>
+        </header>
+
+
+</body>
+
+</html>
